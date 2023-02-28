36ad10c891b08755e6aa4393b645469151fbe32f
Marek Hummel <marek.hummel3@gmail.com>
2023-03-01 00:51:44 +0100

    Solved qr decode kata

---
+++ python/decode_the_qr_code.py
@@ -0,0 +1,70 @@
+from typing import Literal
+
+Bit = Literal[0] | Literal[1]
+
+
+def scanner(qrcode: list[list[Bit]]):
+    bits = get_bits(qrcode)
+    length = bitlist_to_byte(bits[4:12])
+    byte_locations = [12 + offset * 8 for offset in range(length)]
+    message_bits = [bits[loc : loc + 8] for loc in byte_locations]
+    return "".join(chr(bitlist_to_byte(char)) for char in message_bits)
+
+
+def get_bits(qrcode: list[list[Bit]]) -> list[Bit]:
+    bits = []
+    up, zig = True, True
+    x, y = 20, 20  # bottom right
+    while x != 13 or y != 10:
+        mask = (x + y) % 2
+        bits.append(1 - (qrcode[y][x] ^ mask))
+
+        if zig:
+            x -= 1  # left right move
+        elif (y == 9 and up) or (y == 20 and not up):
+            x -= 1  # wanted up or down but reached end of zone
+            up = not up
+        else:
+            x += 1  # up or down
+            y += -1 if up else 1
+
+        zig = not zig
+
+    bits.append(1 - (qrcode[y][x] ^ 1))
+    return bits
+
+
+def bitlist_to_byte(bits: list[Bit]) -> int:
+    value = 0
+    for bit in bits:
+        value = (value << 1) + bit
+    return value
+
+
+print(
+    scanner(
+        [
+            [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+            [1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0],
+            [1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1],
+            [1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+            [1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0],
+            [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1],
+        ]
+    )
+)
